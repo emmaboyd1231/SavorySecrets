66660e4579932dbb68c75b803e2aa6141e46c848
emmaboyd1231 <93304811+emmaboyd1231@users.noreply.github.com>
2022-03-29 12:05:20 -0400

    Set Up for FireBase Database

--- app/src/main/java/com/example/savorysecrets/SignUpActivity.java
+++ app/src/main/java/com/example/savorysecrets/SignUpActivity.java
@@ -6,22 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.savorysecrets.ui.home.HomeFragment;
 import com.example.savorysecrets.R;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity{
 
-    private Button sign_up_account;
+    Button sign_up_account;
+    TextInputLayout email, fName, lName, pword;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         sign_up_account = (Button) findViewById(R.id.sign_up_account);
+
+        email = findViewById(R.id.emailAddress);
+        fName = findViewById(R.id.firstName);
+        lName = findViewById(R.id.lastName);
+        pword = findViewById(R.id.password);
+        
+
         sign_up_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                //Getting all the values
+                String user_email = email.getEditText().getText().toString();
+                String first_name = fName.getEditText().getText().toString();
+                String last_name = lName.getEditText().getText().toString();
+                String user_password = pword.getEditText().getText().toString();
+
+                UserHelperClass helperClass = new UserHelperClass();
+                reference.setValue(helperClass);
                 openNavigationScreen();
             }
         });
